5c34addb311654bd1d5e795d98d5d33a2c5cbce1
skhan61 <sayem.eee.kuet@outlook.com>
2023-09-23 13:09:34 -0400

    Automatic commit at Sat 23 Sep 2023 01:09:34 PM EDT

---
+++ auto_commit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Navigate to your project repository
+cd "/home/sayem/Desktop/Project"
+
+# Check for changes in both working directory and staged area
+if [ -n "$(git diff)" ] || [ -n "$(git diff --cached)" ]; then
+    # Add all changes not mentioned in .gitignore
+    git add .
+
+    # Commit the changes
+    git commit -m "Automatic commit at $(date)"
+
+    # Determine which branch to push based on the current active branch
+    CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+    if [ "$CURRENT_BRANCH" = "main" ]; then
+        git push git@github.com:skhan61/QuantProject.git main
+    else
+        echo "You are currently on the $CURRENT_BRANCH branch. Please switch to the main branch to auto-push."
+    fi
+else
+    echo "No changes detected."
+fi
