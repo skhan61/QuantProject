740dee283a669cfde023ef7d26b9b160350c995e
skhan61 <sayem.eee.kuet@outlook.com>
2023-09-24 23:43:01 -0400

    Automatic commit at Sun 24 Sep 2023 11:43:01 PM EDT

--- push_to_dockerhub.sh
+++ push_to_dockerhub.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
 
-# Read credentials from file
-read -r DOCKER_USER < docker_creds.txt
-read -r DOCKER_PASS
+# Exit on any non-zero status.
+set -e
 
-# Login to Docker Hub
+# 1. Build the Docker container
+sudo docker build -t myproject:latest .
+
+# Check the image size, and ensure it's less than 5GB for any layer.
+image_size=$(sudo docker inspect myproject:latest --format='{{.Size}}')
+if (( $image_size > 5000000000 )); then
+    echo "Image size is greater than 5GB. Exiting."
+    exit 1
+fi
+
+# Print the image size in a human-readable format
+echo "Image size: $(numfmt --to=iec-i --suffix=B $image_size)"
+
+# 2. Get the current date in YYYY-MM-DD format
+current_date=$(date +"%Y-%m-%d")
+
+exec 3<docker_creds.txt
+
+# Read credentials from file
+read -r DOCKER_USER <&3
+read -r DOCKER_PASS <&3
+exec 3<&-
+
+
+# 3. Login to Docker Hub
 echo "$DOCKER_PASS" | sudo docker login -u "$DOCKER_USER" --password-stdin
 
-# Tag your local image with the desired tag
-sudo docker tag myproject:latest k1103061/quantproject_v01:v2023.09
+# 4. Tag your local image with the desired tag using the current date
+tagged_image_name="k1103061/quantproject_v01:$current_date"
+sudo docker tag myproject:latest $tagged_image_name
 
-# Push the newly tagged image to Docker Hub
-sudo docker push k1103061/quantproject_v01:v2023.09
+# 5. Push the newly tagged image to Docker Hub
+echo "Pushing the image to Docker Hub..."
+sudo docker push $tagged_image_name
+
+# 6. Remove the local images to save space after pushing
+sudo docker image rm myproject:latest $tagged_image_name
